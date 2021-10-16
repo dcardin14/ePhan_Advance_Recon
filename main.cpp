2bffbdedc1c28098e4d13913ecca2f252b268279
daniel <dcardin14@gmail.com>
2021-10-16 15:37:54 -0500

    Money formatting
    Now at least it puts the commas in the stream.  I don't know why it
    won't put them in the outputFile, but it's probably better that it
    doesn't anyway.  The put_money didn't work well at all.  For some
    reason, it cuts off the pennies and uses the last 2 dollars as if
    they were pennies.  That's why I'm not really trying to format the
    money currently.

--- main.cpp
+++ main.cpp
@@ -40,34 +40,18 @@ int main() {
           pmtReq[i].advance.balance = pmtReq[i].advance.balance - pmtReq[counter].expenses.amount;  // There will still be a balance
           pmtReq[counter].expenses.amount = 0.0;  //The outer loop expenses are exhaused and applied to inner loop balance.
         }
-        cout.imbue(std::locale("en_US.UTF-8"));
-                cout << setw(15) << std::showbase << put_money(pmtReq[i].advance.appliedAgainst);  //show in terminal what you're applying
-          outputFile << setw(15) << fixed << showpoint << setprecision(2) << std::showbase << put_money(pmtReq[i].advance.appliedAgainst);  // output it to file too
+        cout.imbue(std::locale("en_US.UTF-8"));  //allows me to format these numbers as USD
+                cout << setw(15) << fixed << showpoint << setprecision(2) << pmtReq[i].advance.appliedAgainst;  //show in terminal what you're applying
+          
+          outputFile << setw(15) << fixed << showpoint << setprecision(2) << pmtReq[i].advance.appliedAgainst;  // output it to file too
 
       } //end of the inner loop
       cout << endl;  //Go to the next line for the next payment request
-      outputFile << endl;
-
-
-      // cout << setw(10) << pmtReq[counter].expenses.amount << setw(10) << pmtReq[counter].advance.amount << endl;
-      // outputFile << setw(10) << showpoint << fixed << setprecision(2) << pmtReq[counter].expenses.amount << setw(10) << pmtReq[counter].advance.amount;
-      counter++;
+      outputFile << endl;  //same in the output file
+      counter++;  //increment the while loop counter
     } //end of while
+    outputFile.close();
+    inputFile.close();
 
-    // Create an output file
-
-
-   /* 
-  int earliest;
-  int offset;
-   for(int k = 0; k < MAXPAYMENTS; k++){
-    earliest = findEarliestNonzeroAdvanceBalance(*pmtReq);
-    offset = k - earliest;
-    for(int l = 0; l < offset; l++)
-    {
-     pmtReq[l].advance.amount = pmtReq[l].advance.amount -1; 
-    }
-   }
-*/
     return 0;
 }
